631dc50211b39132ad0e80bed805cff94517597a
wuyoung <463592977@qq.com>
2023-10-18 14:46:11 +0800

    Update densediff.py
    correct mod_forward

--- scripts/densediff.py
+++ scripts/densediff.py
@@ -392,7 +392,7 @@ def mod_forward(self, x, context=None, mask=None, additional_tokens=None,n_times
                 segmask = sreg_maps[sim.size(1)].repeat(h,1,1)
                 size_reg = reg_sizes[sim.size(1)].repeat(h,1,1)
                 
-                sim[index] += (segmask>0)size_reg*sreg*treg*(max_value-sim[index])
+                sim[index] += (segmask>0)*size_reg*sreg*treg*(max_value-sim[index])
                 sim[index] -= ~(segmask>0)*size_reg*sreg*treg*(sim[index]-min_value)
                 if text_cond['model'] != 'sdxl':    
                     mask = sreg_maps[sim.size(1)]
